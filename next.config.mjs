6c720d7d9eb588c7417089bd973f292980145765
maru2katabami <maru2katabami@gmail.com>
2024-03-20 17:38:49 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {}
-export default nextConfig
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})
+
+module.exports = withPWA({
+  reactStrictMode: true,
+})
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
+import NextAuthProvider from "@/lib/nextauth"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={ inter.className }>
-        { children }
+        <NextAuthProvider>
+          { children }
+        </NextAuthProvider>
       </body>
     </html>
   )
